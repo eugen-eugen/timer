bc1029712b3ee7fad0242789d1019a2ccde9ccc6
JeMo <eugen_eugen@gmx.de>
2022-11-29 14:46:45 +0100

    first commit

---
+++ timer.js
@@ -0,0 +1,105 @@
+hours=0;
+minutes=0;
+seconds=0;
+
+function plusHours(){
+    hours++
+    if (hours > 23) {
+        hours=0
+    }
+
+    if (hours < 10){
+        t="0"+hours
+    } else{
+        t=hours
+    }
+
+    document.getElementById("hours").textContent = t
+}
+
+function minusHours(){
+    hours--
+    if (hours < 0) {
+        hours=23
+    }
+
+    if (hours < 10){
+        t="0"+hours
+    } else{
+        t=hours
+    }
+    document.getElementById("hours").textContent=t;
+}
+
+function plusMinutes(){
+    minutes++
+    if (minutes > 59) {
+        minutes=0
+    }
+
+    if (minutes < 10){
+        t="0"+minutes
+    } else{
+        t=minutes
+    }
+
+    document.getElementById("minutes").textContent=t;
+}
+
+function minusMinutes(){
+    minutes--
+    if (minutes < 0) {
+        minusHours()
+        minutes=59
+    }
+
+    if (minutes < 10){
+        t="0"+minutes
+    } else{
+        t=minutes
+    }
+
+    document.getElementById("minutes").textContent=t;
+}
+
+function plusSeconds(){
+    seconds++
+    if (seconds > 59) {
+        seconds=0
+    }
+
+    if (seconds < 10){
+        t="0"+seconds
+    } else{
+        t=seconds
+    }
+
+    document.getElementById("seconds").textContent=t;
+}
+
+function minusSeconds(){
+    seconds--
+    if (seconds < 0) {
+        minusMinutes()
+        seconds=59
+    }
+
+    if (seconds < 10){
+        t="0"+seconds
+    } else{
+        t=seconds
+    }
+
+    document.getElementById("seconds").textContent=t;
+}
+
+function start(){
+    intervalId=setInterval(tick, 1000);
+}
+
+function tick(){
+    minusSeconds()
+    if (seconds == 0 && minutes == 0 && hours == 0){
+        clearInterval(intervalId);
+    }
+}
